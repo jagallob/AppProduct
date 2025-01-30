ae6e7b9b74f9b212dbb63a75cad75fc5b0bdb945
jagallob <jagallob@eafit.edu.co>
2025-01-30 00:59:45 -0500

    Organizar carpetas y separar controladores

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AppProduct.Models;
-using AppProduct.Services;
-using System.Reflection.Metadata.Ecma335;
+
 
 namespace AppProduct.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly IProductService _productService;
 
-    public HomeController(ILogger<HomeController> logger, IProductService productService)
+    public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-        _productService = productService;
     }
 
     public IActionResult Index()
@@ -26,76 +23,6 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Products()
-    {
-        var products = _productService.GetProducts();
-        return View(products);
-    }
-
-    [HttpGet]
-    public IActionResult AddProduct()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public IActionResult AddProduct(Product product)
-    {
-        if (ModelState.IsValid)
-        {
-            _productService.AddProduct(product);
-            return RedirectToAction("Products");
-        }
-        return View(product);
-    }
-
-    // Edit Product Actions
-    [HttpGet]
-    public IActionResult EditProduct(int id)
-    {
-        var product = _productService.GetProductById(id);
-        if (product == null)
-        {
-            return NotFound($"No product found with ID {id}");
-        }
-        return View(product);
-    }
-
-    [HttpPost]
-    public IActionResult EditProduct(Product product)
-    {
-        if (ModelState.IsValid)
-        {
-            _productService.UpdateProduct(product);
-            return RedirectToAction("Products");
-        }
-        return View(product);
-    }
-
-    // Delete Product Actions
-    [HttpGet]
-    public IActionResult DeleteProduct(int id)
-    {
-        var product = _productService.GetProductById(id);
-        if (product == null)
-        {
-            return NotFound($"No product found with ID {id}");
-        }
-        return View(product);
-    }
-
-    [HttpPost, ActionName("DeleteProduct")]
-    public IActionResult DeleteConfirmed(int id)
-    {
-        var product = _productService.GetProductById(id);
-        if (product != null)
-        {
-            _productService.DeleteProduct(id);
-        }
-        return RedirectToAction("Products");
-    }
-
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppProduct.Models;
+using AppProduct.Services;
+
+namespace AppProduct.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _productService.GetProducts();
+        return View(products);
+    }
+
+    [HttpGet]
+    public IActionResult AddProduct()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddProduct(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _productService.AddProduct(product);
+            return RedirectToAction("Index");
+        }
+        return View(product);
+    }
+
+    // Edit Product Actions
+    [HttpGet]
+    public IActionResult EditProduct(int id)
+    {
+        var product = _productService.GetProductById(id);
+        if (product == null)
+        {
+            return NotFound($"No product found with ID {id}");
+        }
+        return View(product);
+    }
+
+    [HttpPost]
+    public IActionResult EditProduct(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _productService.UpdateProduct(product);
+            return RedirectToAction("Index");
+        }
+        return View(product);
+    }
+
+    // Delete Product Actions
+    [HttpGet]
+    public IActionResult DeleteProduct(int id)
+    {
+        var product = _productService.GetProductById(id);
+        if (product == null)
+        {
+            return NotFound($"No product found with ID {id}");
+        }
+        return View(product);
+    }
+
+    [HttpPost, ActionName("DeleteProduct")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var product = _productService.GetProductById(id);
+        if (product != null)
+        {
+            _productService.DeleteProduct(id);
+        }
+        return RedirectToAction("Index");
+    }
+
+}
